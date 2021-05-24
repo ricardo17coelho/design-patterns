e093043f6c422bd2617afe8a5c89c9e15b1f0bc3
Ricardo Coelho <devel@rmorgado.ch>
2021-05-24 12:14:02 +0200

    update Stategy pattern

---
+++ source/src/ZbW.DesignPatterns/Strategy/AbsoluteDiscountOverThresholdStrategy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZbW.DesignPatterns.Strategy
+{
+    public class AbsoluteDiscountOverThresholdStrategy : IPricingStrategy
+    {
+        private readonly decimal _threshold;
+        private readonly decimal _absoluteDiscount;
+
+        public AbsoluteDiscountOverThresholdStrategy(decimal threshold, decimal absoluteDiscount)
+        {
+            _threshold = threshold;
+            _absoluteDiscount = absoluteDiscount;
+        }
+
+        public decimal GetDiscount(Sale sale)
+        {
+            if (sale.Amount >= _threshold)
+            {
+                return _absoluteDiscount;
+            }
+
+            return 0m;
+        }
+    }
+}
---
+++ source/src/ZbW.DesignPatterns/Strategy/PercentageDiscountStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZbW.DesignPatterns.Strategy
+{
+    public class PercentageDiscountStrategy : IPricingStrategy
+    {
+        private readonly decimal _percentage;
+
+        public PercentageDiscountStrategy(decimal percentage)
+        {
+            _percentage = percentage;
+        }
+
+        public decimal GetDiscount(Sale sale)
+        {
+            return sale.Amount / 100m * _percentage;
+        }
+    }
+}
