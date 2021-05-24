c170e6b3ccfc8335bdeeedc43f56e2e1cd4a6435
Ricardo Coelho <devel@rmorgado.ch>
2021-05-24 12:13:28 +0200

    update singleton factory

---
+++ source/src/ZbW.DesignPatterns/Singleton/PrintSpooler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZbW.DesignPatterns.Singleton
+{
+    public class PrintSpooler
+    {
+        private static PrintSpooler _instance;
+
+        private Queue<PrintJob> _printJobs = new Queue<PrintJob>();
+
+        public static PrintSpooler getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new PrintSpooler();
+            }
+
+            return _instance;
+        }
+
+        public void Enqueue(PrintJob printJob)
+        {
+            this._printJobs.Enqueue(printJob);
+        }
+
+        public void Dequeue()
+        {
+            this._printJobs.Dequeue();
+        }
+    }
+}
